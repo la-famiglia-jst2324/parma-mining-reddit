41e6b0c13fad647601bfd77b3d33c1bbd50ade41
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2023-11-28 21:39:59 +0100

    feat: Data retrieval with wrapper (#5)
    Co-authored-by: Ege Kocabas <48245934+egekocabas@users.noreply.github.com>

--- parma_mining/reddit/api/main.py
+++ parma_mining/reddit/api/main.py
@@ -1,6 +1,7 @@
 """Main entrypoint for the API routes in of parma-analytics."""
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from typing import List
+from parma_mining.reddit.client import RedditClient
 
 app = FastAPI()
 
@@ -10,3 +11,14 @@ app = FastAPI()
 def root():
     """Root endpoint for the API."""
     return {"welcome": "at parma-mining-reddit"}
+
+
+@app.post("/get_reddit_data", status_code=status.HTTP_200_OK)
+def get_reddit_data(companies: List[str]) -> dict:
+    if not companies:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Company list is empty!"
+        )
+    reddit_client = RedditClient()
+    results = reddit_client.get_reddit_data(companies)
+    return {"results": results}
